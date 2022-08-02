0664b7531d9ed845702ad4fe662cdd0e10a3b0a0
ankomahene <ankomaheneshadrack@gmail.com>
2022-08-02 14:35:13 +0000

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./styles/App.css";
+import { Form } from "./components/Form";
+import { Header } from "./components/Header";
+import { Results } from "./components/Results";
+import { AppContext } from "./context";
+import { IState } from "./model";
+import { useState } from "react";
+
+const initialState: IState = {
+  principalAmount: 0,
+  interestAmount: 0,
+  totalAmount: 0,
+  monthlyPayment: 0,
+  currency: "GH₵",
+};
 
 function App() {
+  const [state, dispatch] = useState(initialState);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <Header />
       </header>
+      <AppContext.Provider value={{ state, dispatch }}>
+        <main>
+          <section>
+            <div className="info">
+              This is a{" "}
+              <span className="highlight">
+                SIMPLE INTEREST LOAN PAYMENT CALCULATOR.
+              </span>{" "}
+              This calculator will help you determine the monthly payments on a
+              loan. Simply enter the loan amount, term and interest rate in the
+              fields below and click calculate
+            </div>
+          </section>
+
+          <section>
+            <div className="row">
+              <div className="col col-1">
+                <Form />
+              </div>
+              <div className="col col-2">
+                <Results />
+              </div>
+            </div>
+          </section>
+        </main>
+      </AppContext.Provider>
     </div>
   );
 }
---
+++ src/components/Header.tsx
@@ -0,0 +1,3 @@
+export const Header = () => {
+  return <h1 className="header">Loan Payment Calculator</h1>;
+};
